53ffda90a0a6c534cf897abe336fbe606b419806
Importer Script <nobody@localhost>
2017-02-26 17:16:10 +0000

    import: buildroot-GPL-FW1_05_01.tgz
    Origin: http://downloads.wdc.com/gpl/buildroot-GPL-FW1_05_01.tgz

--- primax_packages/wdcore_avatar/sbin/MCU_Temp_Log.sh
+++ primax_packages/wdcore_avatar/sbin/MCU_Temp_Log.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 if [ -d /media/SDcard ]; then
-    echo "`date +%Y/%m/%d-%H:%M:%S`: $1,$2,$3,$4" >> /media/SDcard/MCU_Temp.log
-    cat /tmp/battery >> /media/SDcard/MCU_Temp.log
+    echo "`date +%Y/%m/%d-%H:%M:%S`:" >> /media/SDcard/MCU_Temp.log
+    if [ -f /tmp/MCU_LongReport ]; then
+        echo "31;0" > /tmp/MCU_Cmd
+    fi
+    cat /tmp/batteryPercent >> /media/SDcard/MCU_Temp.log
     cat /tmp/batterymV >> /media/SDcard/MCU_Temp.log
+    cat /tmp/mcuTemperature >> /media/SDcard/MCU_Temp.log
+    if [ -f /tmp/MCU_LongReport ]; then
+        cat /tmp/batterymA >> /media/SDcard/MCU_Temp.log
+        cat /tmp/batteryCycleCount >> /media/SDcard/MCU_Temp.log
+        cat /tmp/batteryRemaining >> /media/SDcard/MCU_Temp.log
+        cat /tmp/batteryRegister >> /media/SDcard/MCU_Temp.log
+    fi
 fi
 
